f203c102914f6c722b4baeaca59ce79c2b486b59
David Stone <davidjamesstone@gmail.com>
2015-09-09 19:33:39 +0100

    Updated example

--- test/bundle.js
+++ test/bundle.js
@@ -3,39 +3,61 @@ var template = require('./template.html')
 
 },{"./template.html":2}],2:[function(require,module,exports){
 module.exports = function (model) {
-elementOpen("div", null, null, "title", data.cls)
+var items = []
+function add (item) {
+  items.push(item)
+}
+
+function remove () {
+  items.pop()
+}
+elementOpen("div", null, null, "title", data.title)
+  elementOpen("div", null, ["class", data.cssClass])
+  elementClose("div")
   text(" \
-    My name is " + data.cls + " my age is " + data.age + " \
-    I live at " + data.address + " \
-    ")
-  elementOpen("span", null, ["title", JSON.stringify(data)])
+  My name is " + data.name + " my age is " + data.age + " \
+  I live at " + data.address + " \
+   \
+  ")
+  elementOpen("span", null, null, "title", JSON.stringify(data))
     text("Hi")
   elementClose("span")
-  elementOpen("input", null, ["type", "text", "onchange", data.onChange], "value", data.val, "style", { color: data.foo, backgroundColor: data.bar })
+  elementOpen("input", null, ["type", "text", "onchange", data.onChange], "value", data.val)
   elementClose("input")
-  if (data.showName) {
+  elementOpen("input", null, ["type", "text", "onchange",  function(e) { data.val = this.value }], "value", data.val)
+  elementClose("input")
+  elementOpen("input", null, ["type", "text", "onchange", function (e) { data.val = this.value }], "value", data.val)
+  elementClose("input")
+  elementOpen("button", null, ["onclick", remove])
+  elementClose("button")
+  if (data.showMe) {
     elementOpen("p")
-      elementOpen("span", null, ["style", { color: data.foo, backgroundColor: data.bar }])
-        text("" + data.name + "")
-      elementClose("span")
       elementOpen("span", null, null, "class", data.bar + ' other-css')
         text("description")
       elementClose("span")
-      elementOpen("span")
-        text("description1")
-      elementClose("span")
-      elementOpen("input", null, ["type", "text"], "disabled", data.isDisabled, "onchange", function() {})
+      elementOpen("input", null, ["type", "text"], "disabled", data.isDisabled)
       elementClose("input")
     elementClose("p")
   }
+  if (data.showMe) {
+    text(" \
+      I'm in an `if` attribute " + basket.totalCost + " \
+    ")
+  }
+  elementOpen("span", null, null, "style", { color: data.foo, backgroundColor: data.bar })
+    text("My style changes")
+  elementClose("span")
+  elementOpen("span", null, ["style", { color: data.foo, backgroundColor: data.bar }])
+    text("My style doesn't change")
+  elementClose("span")
   elementOpen("ul")
-    ;(Array.isArray(data.items) ? data.items : Object.keys(data.items)).forEach(function(item, index) {
-      elementOpen("li", "item in data.items" + index)
-        elementOpen("span")
-          text("" + index + "")
+    ;(Array.isArray(data.items) ? data.items : Object.keys(data.items)).forEach(function(item, $index) {
+      elementOpen("li", $index)
+        elementOpen("span", null, null, "class",  $index % 2 ? 'odd' : 'even' )
+          text("" + $index + "")
         elementClose("span")
         elementOpen("span")
-          text("" + item.name + "")
+          text("" + item.foo + "")
         elementClose("span")
         elementOpen("input", null, null, "value", item.name)
         elementClose("input")
@@ -43,34 +65,40 @@ elementOpen("div", null, null, "title", data.cls)
     }, data.items)
   elementClose("ul")
   elementOpen("ul")
-    ;(Array.isArray(data.obj) ? data.obj : Object.keys(data.obj)).forEach(function(key, index) {
-      elementOpen("li", "key in data.obj" + index)
+    ;(Array.isArray(data.arr) ? data.arr : Object.keys(data.arr)).forEach(function(item, $index) {
+      elementOpen("li", $index)
         elementOpen("span")
-          text("" + key + "")
+          text("" + item + "")
         elementClose("span")
         elementOpen("span")
-          text("" + index + "")
+          text("" + $index + "")
         elementClose("span")
         elementOpen("span")
-          text("" + data.obj[key] + "")
+          text("" + data.arr[item] + "")
         elementClose("span")
       elementClose("li")
-    }, data.obj)
+    }, data.arr)
   elementClose("ul")
   elementOpen("ul")
-    ;(Array.isArray(data.obj) ? data.obj : Object.keys(data.obj)).forEach(function(key, index) {
-      elementOpen("li", "key in data.obj" + index)
+    ;(Array.isArray(data.obj) ? data.obj : Object.keys(data.obj)).forEach(function(key, $index) {
+      elementOpen("li", $index)
         elementOpen("span")
           text("" + key + "")
         elementClose("span")
         elementOpen("span")
-          text("" + index + "")
+          text("" + $index + "")
         elementClose("span")
         elementOpen("span")
           text("" + this[key] + "")
         elementClose("span")
       elementClose("li")
     }, data.obj)
+  elementClose("ul")
+  elementOpen("ul")
+    ;(Array.isArray(data.products) ? data.products : Object.keys(data.products)).forEach(function(product, $index) {
+      elementOpen("li", product.id)
+      elementClose("li")
+    }, data.products)
   elementClose("ul")
 elementClose("div")
 };
